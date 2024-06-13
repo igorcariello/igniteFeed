169cf4bfc36501c4198b4d6069168c439f822e6e
cariello <igor_cariello@hotmail.com.br>
2024-06-12 22:29:24 -0300

    finished Comment and Post components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,43 @@ import './global.css'
 
 import styles from './App.module.css'
 
+//author: { avatar_url: "", name: "", role: """}
+// publishedAt: Date
+//content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/igorcariello.png',
+      name:"Igor Cariello",
+      role: "Frontend Developer"
+    },
+    content:[
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-06-06 23:42:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name:"Diego Fernandes",
+      role: "CTO @Rocketseat"
+    },
+    content:[
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2024-06-08 23:42:00')
+  }
+  
+]
+
+
 
 export function App() {
 
@@ -16,12 +53,18 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         
-        <div className={styles.content}>
-         <Post />
-         <Post />
-
-
-        </div>
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key= {post.id}
+                author= {post.author}
+                content = {post.content}
+                publishedAt = {post.publishedAt}
+              />
+            )
+          })}
+        </main>
       </div>
 
     </ div>
